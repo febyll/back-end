770a54008f6fe02a2f6317f0ceff32b9aac3c3b6
febyll <139439860+febyll@users.noreply.github.com>
2023-10-11 13:35:21 +0530

    Add files via upload

---
+++ febyll/prgm.py
@@ -0,0 +1,53 @@
+# if 5>8:
+#     print("true")
+# else:
+
+#     print("false")
+    
+    
+    
+# i=int(input("enter"))
+# if i>10:
+#     print("greater")
+# elif i<10:
+#     print("not grater than 10") 
+# else:
+#     print("may be equal")     
+    
+    
+# a=50
+# b=18
+# c=10
+# if a>b :
+#     if a>c:
+#         print(a,"is greater")
+#     else:
+#         print(b,"is greater")
+    
+       
+# i=int(input("enter"))
+# if i%7==0:
+#     print("divisible")
+
+# else:
+#     print("not divisible")    
+    
+    
+          
+# i=int(input("enter"))
+# if i%5==0:
+#     print("hello")
+
+# else:
+#     print("bye")   
+    
+    
+          
+i=int(input("enter"))
+m=i%10
+if m%3==0:
+    print("divisible")
+else:
+    print("not divisible")
+print(m)
+
